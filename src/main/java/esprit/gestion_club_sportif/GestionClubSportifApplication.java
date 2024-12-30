ac2464cdb5ee4cebae6dbdd0151fef8c96382eaf
Hadil Hammami <hadilhammami670@gmail.com>
2024-12-30 17:19:44 +0100

    initialize project

---
+++ src/main/java/esprit/gestion_club_sportif/GestionClubSportifApplication.java
@@ -0,0 +1,13 @@
+package esprit.gestion_club_sportif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionClubSportifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionClubSportifApplication.class, args);
+	}
+
+}
---
+++ src/test/java/esprit/gestion_club_sportif/GestionClubSportifApplicationTests.java
@@ -0,0 +1,13 @@
+package esprit.gestion_club_sportif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionClubSportifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
